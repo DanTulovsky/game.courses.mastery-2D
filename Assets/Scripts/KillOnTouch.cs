f221fb4e18459890cab18256525474bd197326f2
Dan Tulovsky <dant@lightstep.com>
2021-12-22 18:56:24 -0500

    death

---
+++ Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        PlayerMovementController player = col.collider.GetComponent<PlayerMovementController>();
+        if (!player) return;
+
+        SceneManager.LoadScene(0);
+    }
+}
