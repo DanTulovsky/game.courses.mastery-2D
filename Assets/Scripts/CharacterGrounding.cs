c0bb9e52cdee4920fe1098aca794179eb317c03e
Dan Tulovsky <dant@lightstep.com>
2021-12-24 12:25:07 -0500

    sticking

--- Assets/Scripts/CharacterGrounding.cs
+++ Assets/Scripts/CharacterGrounding.cs
@@ -9,7 +9,10 @@ public class CharacterGrounding : MonoBehaviour
     [SerializeField] private LayerMask layerMask;
 
     public bool IsGrounded { get; private set; }
+
     private readonly List<Transform> _feet = new();
+    private Transform _groundedObject;
+    private Vector3? _groundedObjectLastPosition;
 
     private void Awake()
     {
@@ -19,12 +22,34 @@ public class CharacterGrounding : MonoBehaviour
 
     private void Update()
     {
-        IsGrounded = CheckIsGrounded();
+        if (!IsGrounded)
+            IsGrounded = CheckIsGrounded();
+        
+        StickToMovingObjects();
+    }
+
+    private void StickToMovingObjects()
+    {
+        if (_groundedObject != null)
+        {
+            if (_groundedObjectLastPosition.HasValue && _groundedObjectLastPosition.Value != _groundedObject.position)
+            {
+                Vector2 delta = _groundedObject.position - _groundedObjectLastPosition.Value;
+                GetComponent<Rigidbody2D>().position += delta; 
+                // transform.position += delta;
+            }
+
+            _groundedObjectLastPosition = _groundedObject.position;
+        }
+        else
+        {
+            _groundedObjectLastPosition = null;
+            return;
+        }
     }
 
     private bool CheckIsGrounded()
     {
-
         foreach (Transform foot in _feet)
         {
             RaycastHit2D hit = Physics2D.Raycast(foot.position, Vector2.down, maxDistance, layerMask);
@@ -33,10 +58,12 @@ public class CharacterGrounding : MonoBehaviour
             // hit.distance check handles the case when jumping through a platform
             if (hit && hit.distance > 0)
             {
+                _groundedObject = hit.collider.transform;
                 return true;
             }
         }
 
+        _groundedObject = null;
         return false;
     }
 }
